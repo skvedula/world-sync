0dd9c356736d4dce574f4eba87322eb6759354f1
skvedula <skvedula@outlook.com>
2016-06-25 15:25:30 +0530

    Initial commit

---
+++ Jscript/app.js
@@ -0,0 +1,24 @@
+var sampleApp = angular.module('sampleApp', []);
+
+sampleApp.config(function($routeProvider) {
+    $routeProvider
+
+        .when('/index', {
+            templateUrl : 'views/register.html',
+            controller  : 'mainController'
+        })
+
+        .when('/register', {
+            templateUrl : 'views/register.html',
+            controller  : 'mainController'
+        })
+
+        .when('/search', {
+            templateUrl : 'views/search-results.html',
+            controller  : 'mainController'
+        });
+});
+
+sampleApp.controller('mainController', function($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+});
